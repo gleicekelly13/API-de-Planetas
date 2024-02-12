c8e870d72791160d5ed748acd92edf5736302cba
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-02-12 14:14:12 -0300

    Create dados.js

---
+++ dados/dados.js
@@ -0,0 +1,13 @@
+const planetas = [
+{ id: 1, nome: 'Mercúrio' },
+{ id: 2, nome: 'Vênus' },
+{ id: 3, nome: 'Terra' },
+{ id: 4, nome: 'Marte' },
+{ id: 5, nome: 'Júpter' },
+{ id: 6, nome: 'Saturno' },
+{ id: 7, nome: 'Urano' },
+{ id: 8, nome: 'Netuno' }
+
+];
+
+exports.planetas = planetas;
