b865198bd5417fa7febbf692b234daf9a32d8815
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-03-07 10:41:51 -0300

    Update services.js

--- services/services.js
+++ services/services.js
@@ -2,14 +2,19 @@ const { planetas } = require('../dados/dados');
 
 function buscarPlanetas() {
     return planetas;
-}
+};
+
+function buscarPlanetasPorNome(nomePlaneta) {
+    return planetas.filter((planeta) => planeta.nome.toLowerCase().includes(nomePlaneta.toLowerCase()));
+};
 
 function buscarPlanetasPorId(id) {
     let idPlanetas = parseInt(id);
     let idPlaneta = planetas.find(planeta => planeta.id === idPlanetas);
 
     return idPlaneta;
-}
+};
 
 exports.buscarPlanetas = buscarPlanetas;
+exports.buscarPlanetasPorNome =buscarPlanetasPorNome;
 exports.buscarPlanetasPorId = buscarPlanetasPorId;
